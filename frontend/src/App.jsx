0c593e6e60bf94d89e13902e12978f86ea1e2c5d
Noah <Noahteitlebaum@gmail.com>
2025-08-31 15:53:22 -0400

    Created components and the search bar for movies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import MovieCard from "./components/MovieCard"
+import NavBar from "./components/NavBar"
+import Favourites from "./pages/Favourites"
+import Home from "./pages/Home"
+import {Routes, Route} from "react-router-dom"
+
+function App() {
+
+  return (
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favourites" element={<Favourites />}/>
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
